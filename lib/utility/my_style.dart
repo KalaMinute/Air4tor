730338b91ca8c900b6fac6ef69a58e1fb0f04a3a
anuwat <kalaminute@gmail.com>
2020-02-29 15:55:10 +0700

    Finish 29Feb

---
+++ lib/utility/my_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Mystyle {
+
+  // Field 
+  Color primaryColor = Color.fromARGB(0xff, 0xfb, 0xc0, 0x2d);
+  Color darkColor = Color.fromARGB(0xff, 0xbc, 0x51, 0x00);
+
+  // Method
+  Mystyle();
+  
+}
--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,3 +1,5 @@
+import 'package:air4tor/utility/my_style.dart';
+import 'package:air4tor/widget/register.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,7 +11,68 @@ class Auther extends StatefulWidget {
 class _AutherState extends State<Auther> {
 // Field
 
-// Method
+///////////////////////////////////// Method
+
+  Widget newRegisterButton() {
+    return FlatButton(
+      child: Text(
+        'New Register',
+        style:
+            TextStyle(color: Mystyle().darkColor, fontStyle: FontStyle.italic),
+      ),
+      onPressed: () {
+        print('You Click Register');
+
+        MaterialPageRoute route =
+            MaterialPageRoute(builder: (BuildContext context) {
+          return Register();
+        });
+        Navigator.of(context).push(route);
+      },
+    );
+  }
+
+  Widget loginButton() {
+    return Container(
+      width: 250.0,
+      child: RaisedButton(
+        color: Mystyle().darkColor,
+        child: Text(
+          'login',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget userForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Mystyle().darkColor)),
+          hintStyle: TextStyle(color: Mystyle().darkColor),
+          hintText: 'User :',
+        ),
+      ),
+    );
+  }
+
+  Widget passForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Mystyle().darkColor)),
+          hintStyle: TextStyle(color: Mystyle().darkColor),
+          hintText: 'Password :',
+        ),
+      ),
+    );
+  }
 
   Widget showLogo() {
     return Container(
@@ -22,8 +85,9 @@ class _AutherState extends State<Auther> {
   Widget showAppName() {
     return Text(
       'Air4tor',
-      style: GoogleFonts.lacquer(textStyle: TextStyle(
-        color: Colors.orange.shade500,
+      style: GoogleFonts.lacquer(
+          textStyle: TextStyle(
+        color: Mystyle().darkColor,
         fontStyle: FontStyle.italic,
         fontWeight: FontWeight.bold,
         fontSize: 30.0,
@@ -31,17 +95,29 @@ class _AutherState extends State<Auther> {
     );
   }
 
+//////////////////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            showLogo(),
-            showAppName(),
-            showAppName(),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: <Color>[Colors.white, Mystyle().primaryColor],
+            radius: 0.9,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showLogo(),
+              showAppName(),
+              userForm(),
+              passForm(),
+              loginButton(),
+              newRegisterButton(),
+            ],
+          ),
         ),
       ),
     );
